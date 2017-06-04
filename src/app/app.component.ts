cceba99c8ac89bbad2c6355d7400edb03db63d9f
Daniel Hernandez <danielhernandez16@hotmail.com>
2017-06-03 23:00:15 -0500

    Create reactive form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 import { TicketService } from './services/ticket.service';
 
+import { FormBuilder, FormGroup } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,6 +14,25 @@ export class AppComponent {
   votacion = '';
   tickets: any;
 
+  // creando grupo de datos para nuestro form reactivo, luego desde hay lo podriamos validar, asignar un parametro por defecto, etc...
+  myForm: FormGroup;
+
+  constructor(
+   // todos los servicios o injecciones que utilizemos hay que declararlas en el componente
+    private ticketService: TicketService,
+    // creando dependencia de nuestro formulario reactivo. El formBUilder es el que hace reactivo nuestro form (lo va a bindear en el momento que se lo pidamos)
+    private fb:FormBuilder
+  ) {
+    this.tickets = ticketService.getTicket();
+
+    // le decimos que va a ser un tipo de formBuilder (array, control o group), en este caso sera group para darle los parametros iniciales
+    this.myForm = fb.group({
+      'name': ['Ecopetrol'],
+      'telefono': ['3116710255']
+    });
+
+  }
+
   votos = [
     {title: 'opcion 1'},
     {title: 'opcion 2'},
@@ -20,10 +41,6 @@ export class AppComponent {
     {title: 'opcion 5'}
   ]
 
-  // todos los servicios o injecciones que utilizemos hay que declararlas en el componente
-  constructor(private ticketService:TicketService) {
-    this.tickets = ticketService.getTicket();
-  }
 
   addVoto(response:string) {
     this.votacion = 'usted eligio la ' + response;
@@ -34,6 +51,10 @@ export class AppComponent {
 
   onSubmit(form:any): void {
     console.log('El formulario tiene', form);
+  }
+
+  onSubmitReactivo(value:string): void {
+    console.log('El formulario tiene', value);
   }
 
 }
